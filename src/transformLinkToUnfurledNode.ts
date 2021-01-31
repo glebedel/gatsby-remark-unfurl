c9427707d6ef0fb7f66fbdbc998aa4271d6117a1
Guillaume Lebedel <guillaume.lebedel@yieldify.com>
2021-01-31 20:59:24 +0000

    feat(template): avoid showing an image if it does not exist

--- src/transformLinkToUnfurledNode.ts
+++ src/transformLinkToUnfurledNode.ts
@@ -3,7 +3,7 @@ import { Node } from 'unist';
 import * as mustache from 'mustache';
 export const tranformsLinkNodeToUnfurledNode = (node: Node, card: MetadataInterface, reporter) => {
   node.type = 'html';
-  const template = `<a class='gatsby-remark-link-unfurl__container' href='{{card.url}}' target='_blank' title='{{card.title}}'><div class='gatsby-remark-link-unfurl__media' style='background-image: url({{card.image.url}})'></div><div class='gatsby-remark-link-unfurl__content'><header class='gatsby-remark-link-unfurl__title'><p  title='{{card.title}}'>{{card.title}}</p></header><div class='gatsby-remark-link-unfurl__description'><p title='{{card.description}}'>{{card.description}}</p></div><footer><p title='{{card.site}}'>{{card.site}}</p><span title='{{card.site}}' style='background-image: url({{card.logo}}); background-repeat: no-repeat; background-position: center center;'></span></footer></div></a>`;
+  const template = `<a class='gatsby-remark-link-unfurl__container' href='{{card.url}}' target='_blank' title='{{card.title}}'>{{#card.image}}<div class='gatsby-remark-link-unfurl__media' style='background-image: url({{card.image.url}})'></div>{{/card.image}}<div class='gatsby-remark-link-unfurl__content'><header class='gatsby-remark-link-unfurl__title'><p  title='{{card.title}}'>{{card.title}}</p></header><div class='gatsby-remark-link-unfurl__description'><p title='{{card.description}}'>{{card.description}}</p></div><footer><p title='{{card.site}}'>{{card.site}}</p><span title='{{card.site}}' style='background-image: url({{card.logo}}); background-repeat: no-repeat; background-position: center center;'></span></footer></div></a>`;
   node.value = mustache.render(template, { card: card });
 
   delete node.children;
